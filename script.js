ac3f9bb3acf47055b95008e63d0c25fe9bf361d1
giooz <giovanni.zapparoli@gmail.com>
2022-07-25 17:16:19 -0300

    created endgame.html

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 //tornar botões de pista opacos no clique
 const checkButton = document.querySelectorAll('.check-button');
 
-for (let i = 0; i < checkButton.length; i++){
-    checkButton[i].addEventListener('click', () => {
-        checkButton[i].classList.toggle('check-button--clicked');
-    })
-}
+checkButton.forEach(button => button.addEventListener('click', () => {
+    button.classList.toggle('check-button--clicked');
+}))
+
+//tornar botões de pista opacos no clique (usando for pra iterar entra cada elemento da array)
+// for (let i = 0; i < checkButton.length; i++){
+//     checkButton[i].addEventListener('click', () => {
+//         checkButton[i].classList.toggle('check-button--clicked');
+//     })
+// }
 
 //esconder a seção de botões ao clicar no botão título
 const sectionTitleButton = document.querySelectorAll('.section-title-button');
 
-for (let i = 0; i < sectionTitleButton.length; i++){
-    sectionTitleButton[i].addEventListener('click', () => {
-        sectionTitleButton[i].parentElement.classList.toggle('hide')
-    })
-}
+sectionTitleButton.forEach(button => button.addEventListener('click', () => {
+    button.parentElement.classList.toggle('hide')
+}))
 
-//esconder a seção de botões ao clicar no botão título (mexendo só no JS)
+//esconder a seção de botões ao clicar no botão título (usando iteração e mexendo só no JS)
 // for (let i = 0; i < sectionTitleButton.length; i++){
 //     sectionTitleButton[i].addEventListener('click', () => {
 //         let nextButton = sectionTitleButton[i].nextElementSibling;
